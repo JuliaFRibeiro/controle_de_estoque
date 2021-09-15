6e8815dd3ba092872c456d14d53861fa813097ab
Julia <julia_felix03@hotmail.com>
2021-09-15 14:36:45 -0300

    Adicionar arquivos de projeto.

---
+++ controle_de_estoque/Produto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace controle_de_estoque
+{
+    public class Produto
+    {
+        // declarando variaveis
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        // construtor
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        // construtor
+        public double ValorTotalEstoque()
+        {
+            // claculo
+            return Preco * Quantidade;
+        }
+
+        // construtor
+        public void AdicionarProduto(int quantidade)
+        {
+            // claculo
+            Quantidade += quantidade;
+        }
+
+        // construtor
+        public void RemoverProdutos(int quantidade)
+        {
+            // claculo
+            Quantidade = Quantidade - quantidade;
+        }
+
+        // construtor
+        public override string ToString()
+        {
+            return Nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: R$ " + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+
+    
+}
